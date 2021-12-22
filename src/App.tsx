86b82e1a672ec867b3628e8f3683c7e8aa3841ca
jinwood <jinw@protonmail.com>
2021-12-22 12:25:16 +0000

    Nouser

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import LoginForm from "./components/common/Form/LoginForm";
 import Layout from "./pages/layout/Default";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/layout/Login";
+import Register from "./pages/layout/Register";
+import NoUser from "./pages/NoUser";
 
 function App() {
   return (
     <div className="App bg-slate-300">
-      <Layout>
-        <LoginForm />
-      </Layout>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/no-user" element={<NoUser />} />
+          </Routes>
+        </Layout>
+      </Router>
     </div>
   );
 }
