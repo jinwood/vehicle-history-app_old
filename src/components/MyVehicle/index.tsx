6d730d0452e77f979f50b1cb68482f0c5f5735f1
jinwood <jinw@protonmail.com>
2022-02-04 18:52:03 +0000

    MyVehicle card

--- src/components/MyVehicle/index.tsx
+++ src/components/MyVehicle/index.tsx
@@ -1,7 +1,34 @@
-export default function MyVehicle() {
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { Vehicle } from "../../store/vehicle";
+
+export default function MyVehicle({ vehicle }: { vehicle: Vehicle }) {
   return (
-    <div>
-      <h1>My Vehicle</h1>
-    </div>
+    <>
+      <Box
+        component="span"
+        sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+      >
+        <CardContent>
+          <Typography
+            component="div"
+            variant="h5"
+            // sx={{ fontSize: 14 }}
+            gutterBottom
+          >
+            Your vehicle
+          </Typography>
+          <Typography component="div" variant="h6">
+            {vehicle.manufacturer} {vehicle.model}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Box>
+    </>
   );
 }
