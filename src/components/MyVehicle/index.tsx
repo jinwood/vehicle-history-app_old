be2b4a0a276469560eb1eb722219596501a690f7
jinwood <jinw@protonmail.com>
2022-02-07 08:39:40 +0000

    Move things around

--- src/components/MyVehicle/index.tsx
+++ src/components/MyVehicle/index.tsx
@@ -1,7 +1,10 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import { Vehicle } from "../../store/vehicle";
 
@@ -10,19 +13,25 @@ export default function MyVehicle({ vehicle }: { vehicle: Vehicle }) {
     <>
       <Box
         component="span"
-        sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+        sx={{
+          display: "inline-block",
+          mx: "2px",
+          transform: "scale(0.8)",
+          width: "100%",
+        }}
       >
-        <CardContent>
-          <Typography component="div" variant="h5" gutterBottom>
-            Your vehicle
-          </Typography>
-          <Typography component="div" variant="h6">
-            {vehicle.manufacturer} {vehicle.model}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
+        <Paper>
+          <Container disableGutters maxWidth="lg" sx={{ pt: 2, pb: 1, pl: 5 }}>
+            <Typography component="div" variant="h5">
+              Your vehicle
+            </Typography>
+          </Container>
+          <Container sx={{ pt: 2, pb: 1 }}>
+            <Typography component="div" variant="h6">
+              {vehicle.manufacturer} {vehicle.model}
+            </Typography>
+          </Container>
+        </Paper>
       </Box>
     </>
   );
