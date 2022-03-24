349b4218f484d17201edb15a8cfb116e0d58a8ba
jinwood <jinw@protonmail.com>
2022-03-24 21:13:50 +0000

    clean up ui

--- src/components/common/Gallery/AddImageDialog.tsx
+++ src/components/common/Gallery/AddImageDialog.tsx
@@ -3,20 +3,29 @@ import Container from "@mui/material/Container";
 import Dialog from "@mui/material/Dialog";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { useSelector } from "react-redux";
 import { uploadFile } from "../../../api/media";
 import { selectVehicle } from "../../../store/slices/vehicleSlice";
+import Button from "@mui/material/Button";
+import styled from "@emotion/styled";
+import IconButton from "@mui/material/IconButton";
+import { PhotoCamera } from "@mui/icons-material";
 
 interface Props {
   open: boolean;
   onClose: () => void;
 }
 
+const Input = styled("input")({
+  display: "none",
+});
+
 export default function VehicleImages(props: Props) {
   const { open, onClose } = props;
   const vehicleState = useSelector(selectVehicle);
   const { vehicle } = vehicleState;
+  const [file, setFile] = useState<File | null>(null);
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -27,11 +36,26 @@ export default function VehicleImages(props: Props) {
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Upload an image of your {vehicle.model}</DialogTitle>
-      <Container>
+      <Container sx={{ p: 2 }}>
         <Typography component="div" variant="h5"></Typography>
         <form onSubmit={handleSubmit}>
-          <input type="file" />
-          <button type="submit">Upload</button>
+          <label htmlFor="contained-button-file">
+            <Input accept="image/*" multiple type="file" />
+            <Button variant="contained" component="span" disabled={!file}>
+              Upload
+            </Button>
+          </label>
+          <label htmlFor="icon-button-file">
+            <Input accept="image/*" id="icon-button-file" type="file" />
+            <Button
+              color="primary"
+              aria-label="upload picture"
+              component="span"
+              endIcon={<PhotoCamera />}
+            >
+              Choose Image
+            </Button>
+          </label>
         </form>
       </Container>
     </Dialog>
