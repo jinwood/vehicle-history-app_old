e56a5c52bcd9862f5b9e754ffea0cd450224811c
jinwood <jinw@protonmail.com>
2022-03-24 21:37:39 +0000

    More tidy

--- src/components/common/Gallery/AddImageDialog.tsx
+++ src/components/common/Gallery/AddImageDialog.tsx
@@ -1,7 +1,6 @@
 import DialogTitle from "@mui/material/DialogTitle";
 import Container from "@mui/material/Container";
 import Dialog from "@mui/material/Dialog";
-import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import { FormEvent, useState } from "react";
 import { useSelector } from "react-redux";
@@ -9,7 +8,6 @@ import { uploadFile } from "../../../api/media";
 import { selectVehicle } from "../../../store/slices/vehicleSlice";
 import Button from "@mui/material/Button";
 import styled from "@emotion/styled";
-import IconButton from "@mui/material/IconButton";
 import { PhotoCamera } from "@mui/icons-material";
 
 interface Props {
@@ -27,10 +25,10 @@ export default function VehicleImages(props: Props) {
   const { vehicle } = vehicleState;
   const [file, setFile] = useState<File | null>(null);
 
-  const handleSubmit = (e: any) => {
+  const onSubmit = (e: FormEvent) => {
     e.preventDefault();
-    console.log(e.target[0].files[0]);
-    uploadFile(e.target[0].files[0], vehicle.uid, "image");
+    if (!file) return;
+    uploadFile(file, vehicle.uid, "image");
   };
 
   return (
@@ -38,22 +36,34 @@ export default function VehicleImages(props: Props) {
       <DialogTitle>Upload an image of your {vehicle.model}</DialogTitle>
       <Container sx={{ p: 2 }}>
         <Typography component="div" variant="h5"></Typography>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={onSubmit}>
           <label htmlFor="contained-button-file">
             <Input accept="image/*" multiple type="file" />
-            <Button variant="contained" component="span" disabled={!file}>
+            <Button
+              variant="contained"
+              component="span"
+              disabled={!file}
+              onClick={onSubmit}
+            >
               Upload
             </Button>
           </label>
           <label htmlFor="icon-button-file">
-            <Input accept="image/*" id="icon-button-file" type="file" />
+            <Input
+              accept="image/*"
+              id="icon-button-file"
+              type="file"
+              onChange={(e) =>
+                setFile(e?.target?.files ? e.target.files[0] : null)
+              }
+            />
             <Button
               color="primary"
               aria-label="upload picture"
               component="span"
               endIcon={<PhotoCamera />}
             >
-              Choose Image
+              {file ? file.name : "Choose Image"}
             </Button>
           </label>
         </form>
